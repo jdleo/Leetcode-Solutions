31717d810cb67b1957dd39a28e09cba094615aad
John Leonardo <j@jdleo.me>
2021-01-03 16:12:17 -0600

    update readme

---
+++ update_problem_count.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+import os
+
+# get problem solved count
+problem_count = os.popen("ls -l solutions/ | wc -l").read()
+problem_count = int(problem_count)
+
+# delete last readme
+os.system("rm README.md")
+
+# write new readme
+with open("README.md", "w") as f:
+    # create readme file contents
+    contents = """# Leetcode Solutions
+All solutions are done in Python. Problems are organized in `/solutions` folder where each corresponding solution is under its problem ID as a folder.  
+
+All solutions are interview-friendly, meaning there is minimal "python golf" and mostly everything can be replicated in most languages
+    
+**Amount Solved**: `%s`
+""" % problem_count
+
+    # write
+    f.write(contents)
+
+# commit
+os.system("git add -A")
+os.system('git commit -m "update readme"')
