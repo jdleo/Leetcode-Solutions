0894be062197bd443acee2753e0da6ec3d0f81f2
john leonardo <j@jdleo.me>
2021-01-03 23:27:00 -0600

    problem #832 completed

--- solutions/832/main.py
+++ solutions/832/main.py
@@ -1,4 +1,4 @@
 class Solution:
     def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
-        # if you see this, i couldnt resist the python golf ;) forgive me
+        # if you see this, i couldnt resist the python golf ;D forgive me
         return [[i ^ 1 for i in row[::-1]] for row in A]
