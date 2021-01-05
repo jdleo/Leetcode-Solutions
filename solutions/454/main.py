a7e98ecc50380dfae9cd270cee8a93e780fa1678
john leonardo <j@jdleo.me>
2021-01-05 09:54:31 -0600

    problem #454 completed

--- solutions/454/main.py
+++ solutions/454/main.py
@@ -1,4 +1,9 @@
+import collections
+import itertools
+
+
 class Solution:
+    # interview friendly (no imports)
     def fourSumCount(self, A: List[int], B: List[int], C: List[int],
                      D: List[int]) -> int:
         # counts for AB pair sums, CD pair sums, and also res
@@ -19,6 +24,21 @@ class Solution:
             # check if there is a b such that x + b = 0. or x = -b
             if -x in CD:
                 # add the count of these two to res
+                res += AB[x] * CD[-x]
+
+        return res
+
+    # shorter but more hand wavy
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int],
+                     D: List[int]) -> int:
+        # res and counts
+        res = 0
+        AB = collections.Counter([a + b for a, b in itertools.product(A, B)])
+        CD = collections.Counter([c + d for c, d in itertools.product(C, D)])
+
+        # go thru pair sums in AB
+        for x in AB:
+            if -x in CD:
                 res += AB[x] * CD[-x]
 
         return res
