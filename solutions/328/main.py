988d9e272e18e75cf236e28fd8d78a401b1d3bce
john leonardo <j@jdleo.me>
2021-01-04 21:52:57 -0600

    problem #328 completed

---
+++ solutions/328/main.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head: return None
+
+        # pointers for odd, even, and dummy head
+        odd = head
+        even = head.next
+        evenHead = even
+
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+
+        odd.next = evenHead
+
+        return head
