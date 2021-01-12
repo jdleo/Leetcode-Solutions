2db9e880e42a737698f910b7ad208812f94b1442
john leonardo <j@jdleo.me>
2021-01-11 23:55:52 -0600

    problem #1325 completed

---
+++ solutions/1325/main.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def removeLeafNodes(self, root: TreeNode, target: int) -> TreeNode:
+        # build left and right subtrees
+        if root.left: root.left = self.removeLeafNodes(root.left, target)
+        if root.right: root.right = self.removeLeafNodes(root.right, target)
+        # delete this node if both left and right are null, AND its target value
+        return None if root.left == root.right and root.val == target else root
