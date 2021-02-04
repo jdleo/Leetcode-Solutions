04abf1a67070a84b28a8c7f723585df634982012
john leonardo <j@jdleo.me>
2021-02-04 16:37:42 -0600

    problem #669 completed

---
+++ solutions/669/main.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        # base case
+        if not root: return None
+
+        # check if out of bounds - if so, skip and go opposite directions
+        if (root.val < low): return self.trimBST(root.right, low, high)
+        if (root.val > high): return self.trimBST(root.left, low, high)
+
+        # set left and right as normal
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+
+        return root
